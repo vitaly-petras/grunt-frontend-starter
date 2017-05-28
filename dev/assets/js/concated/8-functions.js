6587430ba4a7373555c299a857dd066c6c70b557
Viťa <petras@html-factory.cz>
2017-05-28 17:40:24 +0200

    show hide

--- dev/assets/js/concated/8-functions.js
+++ dev/assets/js/concated/8-functions.js
@@ -124,6 +124,31 @@ function toggle(e, element){
     }
 }
 
+//show or hide element
+function showHide(wtt, htd, target, speed){//what to do, how to do
+    var speed = speed || 250;
+
+    if( wtt == "hide" ){
+        if( htd == "slide" ){
+            $(target).stop().slideUp(speed);
+        }else if( htd == "fade" ){
+            $(target).stop().fadeOut(speed);
+        }
+    }else if(wtt == "show"){
+        if( htd == "slide" ){
+            $(target).stop().slideDown(speed);
+        }else if( htd == "fade" ){
+            $(target).stop().fadeIn(speed);
+        }
+    }else if( wtt == "toggle" ){
+        if( htd == "slide" ){
+            $(target).stop().slideToggle(speed);
+        }else if( htd == "fade" ){
+            $(target).stop().fadeToggle(speed);
+        }
+    }
+}
+
 //zamezeni skorolovani
 function bodyFreeze(){
     if( $body.hasClass("overflow") ){//vratit do puvodniho stavu
