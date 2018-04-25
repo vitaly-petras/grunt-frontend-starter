5db8d0a02892724e0af86be3affcc6340e5b0258
Viťa <petras@html-factory.cz>
2018-04-25 13:26:08 +0200

    Changed: object fit images for IE

--- dev/assets/js/concated/object-fit-images-init.js
+++ dev/assets/js/concated/object-fit-images-init.js
@@ -1,1 +1,18 @@
-$(function () { objectFitImages() });
+$(function () {
+	//all normal images
+	var allImages = $('img').not('[data-src]').not('[data-lazy]');
+	objectFitImages(allImages);
+
+
+	//all images lazyloaded
+	var dataSrcImages = $('img[data-src]');
+	var dataLazyImages = $('img[data-lazy]');
+
+	dataSrcImages.on('load', function(){
+		objectFitImages($(this));
+	});
+
+	dataLazyImages.on('load', function(){
+		objectFitImages($(this));
+	});
+});
