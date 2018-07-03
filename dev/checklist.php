8d4d7a16d6f509ec20a4a0882f80b6d3acc0c3aa
Viťa <petras@html-factory.cz>
2018-07-03 09:47:21 +0200

    Changed: checklist title

--- dev/checklist.php
+++ dev/checklist.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-	<title>Přehled stránek</title>
+	<title>Checklist</title>
 
 </head>
 
