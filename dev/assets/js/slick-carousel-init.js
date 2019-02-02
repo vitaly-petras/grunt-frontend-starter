2c0f9b371aed8bf12fb2a3f7614144ffa34a41a0
Viťa <petras@html-factory.cz>
2019-02-02 13:53:18 +0100

    Changed: cleaned javascripts

--- dev/assets/js/slick-carousel-init.js
+++ dev/assets/js/slick-carousel-init.js
@@ -1,54 +1,5 @@
 $(document).ready(function(){
 
-    //pro pouziti obrazku do pozadi pridat na img atribut "data-imgToBG"
-    //priklad: <img src="assets/images/preloader.svg" data-lazy="obrazek" data-imgToBG>
-
-
-    //vetsina karouselu
-    $(".js-carousel").each(function(){
-        var $this = $(this),
-            rows = Number($this.attr("data-rows")),
-            slidesPerRow = Number($this.attr("data-slides-per-row")),
-            respons1 = Number($this.attr("data-resp1")),
-            respons2 = Number($this.attr("data-resp2"));
-
-        $this.slick({
-            dots: false,
-            speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            autoplay: false,
-            arrows:true,
-            rows:rows,
-            lazyLoad: "progressive",
-            slidesPerRow:slidesPerRow,
-            /*responsive:[
-                {
-                    breakpoint: respons1,
-                    settings: {
-                    rows:rows - 1,
-                    initialSlide: Math.floor((Math.random() * itemCount / (rows - 1) + 1)) -1,
-                  }
-                },
-                {
-                    breakpoint: respons2,
-                    settings: {
-                    rows:rows - 2,
-                    initialSlide: Math.floor((Math.random() * itemCount / (rows - 2) + 1)) -1,
-                  }
-                },
-                {
-                    breakpoint: 639,
-                    settings: {
-                    rows:1,
-                    initialSlide: initSlide,
-                  }
-                },
-            ]*/
-        });
-    });
-
-
     //intro carousel
     $("#js-intro-carousel").slick({
         dots: true,
