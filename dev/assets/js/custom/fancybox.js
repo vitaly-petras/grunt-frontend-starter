8ce18046e043b5d083ace4803e85d5bc8c77b6a9
Viťa <petras@html-factory.cz>
2019-10-10 12:55:31 +0200

    Fixed: fancybox

--- dev/assets/js/custom/fancybox.js
+++ dev/assets/js/custom/fancybox.js
@@ -1,26 +1,27 @@
 $(function() {
-  var fancyboxIsLoaded = false;
-  var fancyboxISloading = false;
-
   function doFancyboxFunctions(event) {
     var loadFancybox = function() {
-      //load css
-      var head = document.getElementsByTagName("head")[0];
-      var link = document.createElement("link");
-      link.id = "fancybox-css";
-      link.rel = "stylesheet";
-      link.type = "text/css";
-      link.href = "https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css"; //"/assets/css/fancybox.css";
-      link.media = "all";
-      $(head).prepend(link);
+      if ($("#fancybox-css").length < 1) {
+        //load css
+        var head = document.getElementsByTagName("head")[0];
+        var link = document.createElement("link");
+        link.id = "fancybox-css";
+        link.rel = "stylesheet";
+        link.type = "text/css";
+        link.href = "https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css"; //"/assets/css/fancybox.css";
+        link.media = "all";
+        $(head).prepend(link);
+      }
 
-      //load script
-      var resource = document.createElement("script");
-      resource.async = "true";
-      resource.src = "https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"; //"/assets/js/fancybox.js";
-      resource.id = "fancybox-js";
-      var script = document.getElementsByTagName("script")[0];
-      script.parentNode.insertBefore(resource, script);
+      if ($("#fancybox-js").length < 1) {
+        //load script
+        var resource = document.createElement("script");
+        resource.async = "true";
+        resource.src = "https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"; //"/assets/js/fancybox.js";
+        resource.id = "fancybox-js";
+        var script = document.getElementsByTagName("script")[0];
+        script.parentNode.insertBefore(resource, script);
+      }
     };
 
     var setupFancybox = function() {
@@ -30,11 +31,11 @@ $(function() {
     };
 
     var isFancyboxLoaded = function() {
-      return !fancyboxIsLoaded && !fancyboxISloading ? false : true;
+      return $("html").hasClass("fancybox-css-loaded") && $("html").hasClass("fancybox-js-loaded");
     };
 
     var startFancybox = function() {
-      fancyboxIsLoaded = true;
+      $("html").addClass("fancybox-is-loaded");
 
       //nepokracuj pokud css nebo js neni nacteny
       setupFancybox();
@@ -47,15 +48,16 @@ $(function() {
 
     if (event && event.type === "click" && isFancyboxLoaded() === false) event.preventDefault();
 
-    if (isFancyboxLoaded() === false) {
+    if (!isFancyboxLoaded()) {
       loadFancybox();
-      fancyboxISloading = true;
 
       $("#fancybox-css").on("load", function() {
+        $("html").addClass("fancybox-css-loaded");
         if (isFancyboxLoaded()) startFancybox();
       });
 
       $("#fancybox-js").on("load", function() {
+        $("html").addClass("fancybox-js-loaded");
         if (isFancyboxLoaded()) startFancybox();
       });
     }
